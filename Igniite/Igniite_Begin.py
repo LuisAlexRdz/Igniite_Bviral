a565c03fbfb42ed9cfbdac5a96dcf3d40652f5dd
LuisAlexRdz <rdz.alex83@gmail.com>
2023-05-08 18:51:35 -0600

    08/05/2023

--- Igniite/Igniite_Begin.py
+++ Igniite/Igniite_Begin.py
@@ -12,5 +12,27 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.firefox.options import Options
 from Funciones import Funciones_Globales
 from  selenium.webdriver import ActionChains
-t= 1
+t= 5
 
+def get_Data():
+    return [
+        ("AlexRdzPdr", "Alex1983")
+    ]
+
+
+@pytest.mark.parametrize("user,clave", get_Data())
+def test_login(user,clave):
+    global  driver, f
+    driver = webdriver.Chrome(executable_path="Drivers/chromedriver.exe")
+    driver.get("https://test.igniite.io/login")
+    driver.maximize_window()
+    driver.implicitly_wait(20)
+    f = Funciones_Globales(driver)
+    f.Texto_Mixto("xpath","//input[contains(@id,'user')]", user, t)
+    f.Texto_Mixto("xpath","//input[contains(@id,'password')]", clave, t)
+    f.Click_Mixto("xpath","//button[@type='submit'][contains(.,'LOGIN')]",t)
+    print("Entrando al sistema")
+
+def teardown_function():
+    print("Salida del test")
+    driver.close()
